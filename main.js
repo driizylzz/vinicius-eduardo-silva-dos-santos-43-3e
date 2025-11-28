58efc00728fa2f64385b0e6e7615b41bc4d7e3ee
driizylzz <148235894+driizylzz@users.noreply.github.com>
2025-11-27 21:22:15 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const tempos=[ tempoobjetivos,tempoobjetivo1,tempoobjetivo3,tempoobjetivom4];
+
+function calculatempo(tempoobjetivo){
+    let tempoatual= new Date();
+    let tempofinal= tempoobjetivo-tempoatual;
+    let segundos= Math.floor(tempofinal/1000);
+    let minutos= Math.floor(segundos/60);
+    let horas= Math.floor(horas/24);
+    segundos%=60;
+    minutos%=60;
+    horas%=24;
+}
+
+if(tempofinal) {
+    
+    
+}
